11eaadccb4394b3906b60a5f15a03206639d35eb
Aaron Covington <TheAaronCovington@gmail.com>
2015-04-27 19:30:48 -0500

    Update 03LongestWord.java

--- 03LongestWord.java
+++ 03LongestWord.java
@@ -1,3 +1,9 @@
+/*
+Description: Using the Java language, have the function LongestWord(sen) take the sen parameter being passed and 
+return the largest word in the string. If there are two or more words that are the same length, return 
+the first word from the string with that length. Ignore punctuation and assume sen will not be empty. 
+*/
+
 import java.util.*; 
 import java.io.*;
 
