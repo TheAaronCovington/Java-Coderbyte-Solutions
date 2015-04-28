1850963e5c5ca647f56a873dbcfa1d5c4a8d5477
Aaron Covington <TheAaronCovington@gmail.com>
2015-04-27 19:28:58 -0500

    Update 02FirstFactorial.java

--- 02FirstFactorial.java
+++ 02FirstFactorial.java
@@ -1,3 +1,9 @@
+/*
+Problem Description: Using the Java language, have the function FirstFactorial(num) take the num parameter 
+being passed and return the factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, 
+the range will be between 1 and 18. 
+*/
+
 import java.util.*; 
 import java.io.*;
 
