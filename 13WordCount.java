86a30653841faeab3b5535d4e32f559fc62ca909
Aaron Covington <TheAaronCovington@gmail.com>
2015-05-04 04:19:09 -0500

    Update 13WordCount.java

--- 13WordCount.java
+++ 13WordCount.java
@@ -3,6 +3,11 @@
  * take the str string parameter being passed and return the number of 
  * words the string contains (ie. "Never eat shredded wheat" would return 4).
  * Words will be separated by single spaces. 
+ * Note: After completing this problem I checked another users solution and found a more optimal way of doing this:
+	String[] myArray = str.split(" ");
+	Integer count = myArray.length;
+	str = count.toString();
+	return str;
  */
 
 import java.util.*; 
