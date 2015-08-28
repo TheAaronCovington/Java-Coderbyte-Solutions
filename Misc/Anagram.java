96d55c1476021c814b09ff0525c41041d54700b9
Aaron Covington <TheAaronCovington@gmail.com>
2015-08-27 23:05:30 -0500

    Updated to remove spaces

--- Misc/Anagram.java
+++ Misc/Anagram.java
@@ -7,15 +7,15 @@ public class Anagram {
     private static char[] string1, string2;
 
     public static boolean isAnagram(String str1, String str2){
-        string1 = str1.toLowerCase().toCharArray();
-        string2 = str2.toLowerCase().toCharArray();
+        string1 = str1.replaceAll(" ", "").toLowerCase().toCharArray();
+        string2 = str2.replaceAll(" ", "").toLowerCase().toCharArray();
         Arrays.sort(string1);
         Arrays.sort(string2);
         return Arrays.equals(string1, string2);
     }
 
     public static void main(String[] args){
-        System.out.println(Anagram.isAnagram("live", "evil"));
+        System.out.println(Anagram.isAnagram("li ve", "evi l"));
         System.out.println(Anagram.isAnagram("david", "dave"));
     }
 }
